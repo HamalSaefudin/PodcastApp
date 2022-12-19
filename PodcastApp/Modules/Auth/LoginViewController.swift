4e3b526ba0947f03284b7210c4e4d96d0ffb2df2
Hamal <Hamalsaefudin48@gmail.com>
2022-12-19 21:50:56 +0700

    create sign in and sign up form

---
+++ PodcastApp/Modules/Auth/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  PodcastApp
+//
+//  Created by Hamal Saefudin on 19/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+}
