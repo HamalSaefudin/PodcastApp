d89f541ac6387bab80e633c30667375aa8824351
Hamal <Hamalsaefudin48@gmail.com>
2022-12-19 18:51:49 +0700

    Initial Commit

---
+++ PodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by Hamal Saefudin on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
